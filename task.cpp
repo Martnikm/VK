a7cb66cb12d161f862997383bd86113f1575ae38
Martnikm <72187944+Martnikm@users.noreply.github.com>
2025-03-17 13:13:24 +0300

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -5,22 +5,22 @@
 
     void BFS(const std::vector<std::vector<int> > &graph, int start, std::vector<int> &distance) {
         int n = graph.size();
-        std::queue<int> q;
+        std::queue<int> Queue;
         std::vector<bool> visited(n, false);
 
-        q.push(start);
+        Queue.push(start);
         visited[start] = true;
         distance[start] = 0;
 
-        while (!q.empty()) {
-            int v = q.front();
-            q.pop();
+        while (!Queue.empty()) {
+            int node = Queue.front();
+            Queue.pop();
 
-            for (int neighbor : graph[v]) {
+            for (int neighbor : graph[node]) {
                 if (!visited[neighbor]) {
                     visited[neighbor] = true;
-                    distance[neighbor] = distance[v] + 1;
-                    q.push(neighbor);
+                    distance[neighbor] = distance[node] + 1;
+                    Queue.push(neighbor);
                 }
             }
         }
@@ -29,31 +29,29 @@
     int main() {    
         std::ifstream file("graph.txt");
         if (!file) {
-            std::cerr << "Error" << std::endl;
+            std::cerr << "Error. Graph.txt not found." << std::endl;
             return 0;
         }
 
-        int ver = 0, edges = 0;
-        file >> ver >> edges;
-        std::vector<std::vector<int> > graph(ver);
+        int nodes = 0, edges = 0;
+        file >> nodes >> edges;
+        std::vector<std::vector<int> > graph(nodes);
 
         for(int i = 0; i < edges; ++i) {
-            int u = 0, v = 0;
-            file >> u >> v;
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+            int firstNode = 0, secondNode = 0;
+            file >> firstNode >> secondNode;
+            graph[firstNode].push_back(secondNode);
+            graph[secondNode].push_back(firstNode);
         }
 
         int start = 0;
         file >> start;
         file.close();
-        std::vector<int> distance(ver, -1);
+        std::vector<int> distance(nodes, -1);
 
         BFS(graph, start, distance);
 
-        for(int i = 0; i < ver; ++i) {
-            std::cout << distance[i] << std::endl;
-        }
+        for(int i = 0; i < nodes; ++i) std::cout << distance[i] << std::endl;
 
         return 0;
     }
